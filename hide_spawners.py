c15188f051af4586d1c3407a51532dfbe28e2554
Chris Sprance <chriss@marketingprovisions.com>
2014-11-24 20:04:21 -0500

    updated doesn't seem to work with objects named the same things

--- hide_spawners.py
+++ hide_spawners.py
@@ -4,7 +4,8 @@
 # Entrada Interactive
 # Miscreated 
 # ##################################################
-
+#
+import general
 # function to get the list of current spawners
 def getSpawners():
 	import os
@@ -19,6 +20,40 @@ def getSpawners():
 			spawnerlist.append(x.replace('.cgf', ''))
 	return spawnerlist
 
+# clean objects list and returns a list of a list with name and model geometry used
+def cleanObjects(obj):
+	#return list
+	retList = list()
+	for x in obj:
+		#run through our objects and only return the brushes
+		if general.get_object_type(x) == 'Brush':
+			objlist = list()
+			#split the list by / and grab the last then split that by . and grab the first
+			model = general.get_entity_param(x, 'Geometry').split('/')[-1].split('.')[0]
+			# add those to the retlist as a dictionary ('Name of Object': 'ModelGeo')
+			objlist.append(model)
+			objlist.append(x)
+			retList.append(objlist)
+	#send it all back
+	return retList
+
+#function to get selected objects in the layer
+def getSelectedObjects(selection):
+	sel = selection
+	objects = list()
+	objects.extend(sel)
+	for x in sel:
+		# Go find it's children objects and add that to the return list
+		objects.extend(findChild(x))
+	return cleanObjects(objects)
+
+# This function goes through an objects and finds if it has child objects and return them in a list
+def findChild(parent):
+	# get the children object from the parent
+	children = general.get_object_children(parent)
+	#return our list of children
+	return children
+
 # function to get all the objects in the level into one list
 def getAllObjects():
 	# define our list of objects to search through
@@ -29,44 +64,58 @@ def getAllObjects():
 	for x in layers:
 		objects.extend(general.get_all_objects_of_layer(x))
 	#send the compiled list back
-	return objects	
+	return cleanObjects(objects)	
+
+#function to find out what selection to do
+def getObjects():
+	selection = general.get_names_of_selected_objects()
+	# test if we have anything selected and warn the user about each operation
+	if len(selection) == 1:
+		return getSelectedObjects(selection)
+	elif len(selection) == 0: 
+		sure = general.message_box('You have no objects selected which means you will be will hiding all. This will probably crash on islands. Are you sure?')
+		if sure == True:
+			return getAllObjects()
+	elif len(selection) > 15:
+		sure = general.message_box('You have a large number of items selected. This may crash, make sure you save first.')
+		if sure == True:
+			return getSelectedObjects(selection)
+	else:
+		return getSelectedObjects(selection)
 
 # function to hide an object
 def hideObject(obj):
-	general.hide_objects(obj)
+	print obj
+	for x in obj:
+		general.hide_object(x)
+		#print "Hiding: "+x
 
 # function to unhide an object
 def unhideObject(obj):
-	general.unhide_object(obj)
+	for x in obj:
+		general.unhide_object(x)
+			
 
-	
+
+
+# import our regex module
+import re
+
 # get our spawners
 spawners = getSpawners()
 # get our objects
-objects = getAllObjects()
-
-for obj in objects:
-	for spawner in spawners:
-		# print spawner
-		# print obj
-		if obj.find(spawner):
-			print obj
-		# here I need to compare the spawner to the obj and see if the names match
-		# probably need to format the obj first and remove and number maybe using obj.find(spawner)
-# the list full of the objects we want to hide
+objects = getObjects()
+#print objects
+# create our list to store the hide items in
 hidelist = list()
-
-#iterate through our objects and check them against the spwaners list
-
-			
-# 
-# 
-# for each object in objects
-# 	if it matches one of the valuesin spwaners[]
-# 	add it to spawnerlist[]
-# 
-# 
-# for x in spawnerlist:
-# 	general.hide_object(x)
-# 
-
+#generate our hide lists by iterating through our objects and spawners
+for spawner in spawners:
+	# create our regex string
+	regex = re.escape(spawner)
+	# loop through each object
+	for obj in objects:		
+		#test and see if our regex matches
+		if re.search(regex, obj[0]):
+			hidelist.append(obj[1])
+# hide the objects
+hideObject(hidelist)
